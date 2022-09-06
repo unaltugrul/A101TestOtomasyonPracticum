b1ad1a7fd3ba159b3c7f9ab18c4dda917adb7974
Unal <unaltugrulbulut@gmail.com>
2022-09-06 15:00:30 +0300

    Precondition and TC001 snippets

--- src/test/java/com/a101/step_definitions/Odeme_ekrani_test_steps.java
+++ src/test/java/com/a101/step_definitions/Odeme_ekrani_test_steps.java
@@ -1,5 +1,34 @@
 package com.a101.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
 public class Odeme_ekrani_test_steps {
+
+    @Given("Kullanici {string} adresine yonlenir")
+    public void kullanici_adresine_yonlenir(String string) {
+
+    }
+    @Then("Kullanicinin basarili bir sekilde ana sayfaya ulasir")
+    public void kullanicinin_basarili_bir_sekilde_ana_sayfaya_ulasir() {
+
+    }
+    @Given("Kullanici giyim ve aksesuar modulu uzerinde fareyi bekletir")
+    public void kullanici_giyim_ve_aksesuar_modulu_uzerinde_fareyi_bekletir() {
+
+    }
+    @Given("Kullanici acilan menuden Kadin Ic Giyim ve Dizalti Corap kategorisine tiklar")
+    public void kullanici_acilan_menuden_kadin_ic_giyim_ve_dizalti_corap_kategorisine_tiklar() {
+
+    }
+    @Given("Kullanici urun listesindeki ilk urunu tiklar")
+    public void kullanici_urun_listesindeki_ilk_urunu_tiklar() {
+
+    }
+    @Then("Kullanici acilan urunun siyah oldugunu gorur")
+    public void kullanici_acilan_urunun_siyah_oldugunu_gorur() {
+
+    }
+
 
 }

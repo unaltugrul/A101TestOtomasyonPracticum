fe6f01b1a24966823287965b3c8a5625a875ce18
Unal <unaltugrulbulut@gmail.com>
2022-09-07 11:03:08 +0300

    PenKad50DenPanCorSiyah.java created and color element added

---
+++ src/test/java/com/a101/pages/PenKad50DenPanCorSiyah.java
@@ -0,0 +1,17 @@
+package com.a101.pages;
+
+import com.a101.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PenKad50DenPanCorSiyah extends BasePage{
+    public PenKad50DenPanCorSiyah(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//div[@class='selected-variant-text']//span")
+    public WebElement renk;
+
+
+}

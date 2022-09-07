70377431e721985a8feb80caecab6880fb587196
Unal <unaltugrulbulut@gmail.com>
2022-09-07 10:17:14 +0300

    Driver.java arranged for notifications

--- src/test/java/com/a101/utilities/Driver.java
+++ src/test/java/com/a101/utilities/Driver.java
@@ -3,15 +3,7 @@ package com.a101.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
@@ -23,24 +15,13 @@ public class Driver {
 
     public static WebDriver getDriver() {
         if (driverPool.get() == null) {
-
             String browserType = ConfigurationReader.getProperty("browser");
-
-            switch (browserType) {
-                case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    driverPool.set(new ChromeDriver());
-                    driverPool.get().manage().window().maximize();
-                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                    break;
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driverPool.set(new FirefoxDriver());
-                    driverPool.get().manage().window().maximize();
-                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                    break;
-
-            }
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-notifications");
+            WebDriverManager.chromedriver().setup();
+            driverPool.set(new ChromeDriver(options));
+            driverPool.get().manage().window().maximize();
+            driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }
         return driverPool.get();
     }

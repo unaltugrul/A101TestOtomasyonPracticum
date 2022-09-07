187dae828f657194cd1caa331790306027e5dff7
Unal <unaltugrulbulut@gmail.com>
2022-09-07 12:58:49 +0300

    SepetPage.java added

---
+++ src/test/java/com/a101/pages/SepetPage.java
@@ -0,0 +1,15 @@
+package com.a101.pages;
+
+import com.a101.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SepetPage extends BasePage{
+    public SepetPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//div[@class='basket-list']//a[@class='title']")
+    public WebElement eklenenUrun;
+}

a4af654b5b4a17792b99a1b00b8c3a6d1240a3d4
Unal <unaltugrulbulut@gmail.com>
2022-09-12 08:38:01 +0300

    updating

--- src/test/java/com/a101/runners/CukesRunner.java
+++ src/test/java/com/a101/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/a101/step_definitions",
         dryRun = false,
-        tags = "",
+        //tags = "",
         publish = true
 
 )
